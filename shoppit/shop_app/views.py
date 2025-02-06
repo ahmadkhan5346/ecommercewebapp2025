51066a1fe2cfe698e69a6b5cea72990f34e09e29
ahmadkhan5346 <khansarfraz5346@gmail.com>
2025-02-06 12:12:51 +0530

    create a product feature

--- shoppit/shop_app/views.py
+++ shoppit/shop_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from shop_app.models import Product
+from shop_app.serializers import ProductSerializer
+from rest_framework.response import Response
 # Create your views here.
+
+@api_view("GET")
+def products(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
